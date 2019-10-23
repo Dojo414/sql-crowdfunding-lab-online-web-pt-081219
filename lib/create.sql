5caad35d626d4cca100e88f90e4d321c337b979c
Donovan Jones <donovanljones94@gmail.com>
2019-10-23 20:09:39 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,4 +1,4 @@
-CREATE TABLE projects (
+CREATE TABLE projects(
 id INTEGER PRIMARY KEY,
 title TEXT,
 category TEXT,
@@ -7,7 +7,7 @@ start_date TEXT,
 end_date TEXT,
 );
 
-CREATE TABLE users (
+CREATE TABLE users(
 id INTEGER PRIMARY KEY,
 name TEXT,
 age INTEGER);
