7a1a85ab3a899bc5a2ada124726e0591d4d2beb8
Donovan Jones <donovanljones94@gmail.com>
2019-10-24 13:12:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,9 +21,9 @@ def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT project.title, (SUM(pledges.amount) - pledges.funding_goal) AS over_goal FROM projects
+  "SELECT projects.title, (SUM(pledges.amount) - pledges.funding_goal) AS over_goal FROM projects
    INNER JOIN pledges
-   ON project.id = pledge.project_id
+   ON projects.id = pledges.project_id
    GROUP BY project.title
    HAVING over_goal >= 0;"
 end
